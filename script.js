3af3178db04c83a00fe02a86672c9a18706160e1
kelvinachiando <kelvinostin22gmail.com>
2022-06-25 20:31:05 +0300

    added a public API from which the data will be fetched

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ search_btn.addEventListener('onclick', e=> {
 
 const word=input.value;
 
-const fetchData = async () =>
+const fetchData = async () => {
+    const input = document.querySelector('input').value;
+    console.log(input);
+    if(!!input){
+        try{
+            const response =  await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
+            const responseJson = await response.json();
+            console.log(responseJson); 
+            document.getElementById('result').innerHTML = responseJson[0].meanings[0].definitions[0].definition; 
+        }
+        catch(e){
+            console.error('There was an error during fetch ', e);
+        }
+    }
+}
+
